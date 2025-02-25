2c02c7a13585542427d20c9ae3ef5ed2dfb4a54a
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2025-02-25 22:57:18 +0530

    Add replicasets

---
+++ apps/timer/main.py
@@ -0,0 +1,20 @@
+import time
+
+
+def main():
+    print("Starting the timer")
+    print("Going for some sleep")
+    time.sleep(4)
+    print("Waking up")
+    print(time.ctime())
+
+    #while True:
+    #    print(time.ctime())
+    #    time.sleep(2)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
